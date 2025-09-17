c4777fc21279564a790e6099bd08f7d646f2acb3
Rasmus Koch Jensen <rasmuskochj@gmail.com>
2025-09-17 23:33:12 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -7,4 +7,12 @@ out="0,3,5;"
 
 [[ $(./cmd_int <<< "$in") == "$out"* ]] && echo "PASSED" || echo "FAILED"
 
+in1="abbbbbbbbbbaccaq"
+out1="14;"
 
+[[ $(./cmd_int <<< "$in1") == "$out1"* ]] && echo "PASSED" || echo "FAILED"
+
+in2="aabbbaqtest"
+out2="0,1,5;"
+
+[[ $(./cmd_int <<< "$in2") == "$out2"* ]] && echo "PASSED" || echo "FAILED"
